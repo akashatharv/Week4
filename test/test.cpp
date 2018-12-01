ef01b2229b825e70418f3e739c4ba3d37f83fa3a
Nithish <nithishtimes@gmail.com>
2018-11-30 21:48:45 -0500

    Updated CMakefile to accomodate the new dummy class

--- test/test.cpp
+++ test/test.cpp
@@ -10,7 +10,7 @@
 
 #include <gtest/gtest.h>
 #include "PID.hpp"
-
+#include "level.hpp"
 /** @brief Test to verify if compute function of classPID is working
  * properly or not
  *
@@ -27,8 +27,11 @@ TEST(pidCompute, should_pass) {
 */
 TEST(gainEnquiry, should_pass) {
   PID obj;   /*!< Object of the class PID  */
+  levelAlarm obj1;
+  obj1.levelControl();
   obj.inputGains(0.5,0.2,0.1);
   EXPECT_EQ(0.5, obj.returnKp());
   EXPECT_EQ(0.2, obj.returnKi());
   EXPECT_EQ(0.1, obj.returnKd());
+  EXPECT_EQ(1, obj1.returnAlarm());
 }
