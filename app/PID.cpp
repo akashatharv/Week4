700eaec91a4c2177a8874914982fdc7f2aae5bf6
Nithish <nithishtimes@gmail.com>
2018-12-01 00:54:21 -0500

    app/PID.cpp Refactored the class for mock

--- app/PID.cpp
+++ app/PID.cpp
@@ -6,18 +6,25 @@
  * @copyright 2018, RajendraMayavan
  */
 #include "PID.hpp"
+#include "iostream"
+PID::PID(){
+  Kp = 0;
+  Kd = 0;
+  Ki = 0;
 
+}
 double PID::computeVelocity(double targetSetpoint, double velocity) {
     // Calculates the error to be corrected
     double error = targetSetpoint - velocity;
     const double dt = 0.2;
     // Calculates the output velocity using PID constants
     double output = error * Kp + (error/dt) * Kd + (error*dt) *Ki;
+    std::cout<<"Output is:"<<output;
     return output;
 }
 
-void PID::inputGains(const double inputKp,
-                     const double inputKi, const double inputKd) {
+void PID::inputGains(double inputKp,
+                     double inputKi, double inputKd) {
     Kp = inputKp;
     Kd = inputKd;
     Ki = inputKi;
@@ -34,3 +41,5 @@ double PID::returnKi() {
 double PID::returnKd() {
     return Kd;
 }
+PID::~PID(){}
+
--- app/level.cpp
+++ app/level.cpp
@@ -1,5 +1,3 @@
-
-
 #include "level.hpp"
 #include "iostream"
 
